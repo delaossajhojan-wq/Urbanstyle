c71d0aac91b6b680f12fe03c045c397445e9d767
delaossajhojan-wq <delaossajhojan@gmail.com>
2026-03-26 14:46:17 -0500

    Add files via upload

---
+++ tienda.js
@@ -0,0 +1,125 @@
+const productos = [
+    { id: 1, nombre: "Camiseta oversized", precio: 20000, img: "Oversized1.jpg" },
+    { id: 2, nombre: "Pantalón", precio: 67000, img: "pantalon.jpg" },
+    { id: 3, nombre: "Chaqueta de cuero", precio: 80000, img: "chaqueta hombre.jpg" },
+    { id: 4, nombre: "Camiseta", precio: 28000, img: "CamisetasBasic.jpg" },
+    { id: 5, nombre: "jean blue", precio: 55000, img: "panta.jpg" },
+    { id: 6, nombre: "Chaqueta", precio: 67000, img: "chaqueta marron.jpg" },
+    { id: 7, nombre: "Camiseta", precio: 20000, img: "negra.jpg" },
+    { id: 8, nombre: "Pantalón", precio: 46000, img: "Pantalon1.jpg" },
+    { id: 9, nombre: "Chaqueta de jean", precio: 38000, img: "66b6e473fd5e4aa1b627e1bbc432f033.jpg" }
+];
+
+let carrito = [];
+let pedidos = [];
+const inputBusqueda = document.getElementById("input-busqueda");
+
+const contenedor = document.getElementById("lista-productos");
+
+productos.forEach(p => {
+    contenedor.innerHTML += `
+        <div class="producto">
+            <img src="${p.img}">
+            <h3>${p.nombre}</h3>
+            <p>$${p.precio}</p>
+            <button onclick="agregarCarrito(${p.id})">Comprar</button>
+        </div>
+    `;
+});
+
+
+function agregarCarrito(id) {
+    const producto = productos.find(p => p.id === id);
+    carrito.push(producto);
+    actualizarCarrito();
+}
+
+
+function actualizarCarrito() {
+    const lista = document.getElementById("items-carrito");
+    const total = document.getElementById("total");
+    const contador = document.getElementById("contador");
+
+    lista.innerHTML = "";
+    let suma = 0;
+
+    carrito.forEach(p => {
+        lista.innerHTML += `<li>${p.nombre} - $${p.precio}</li>`;
+        suma += p.precio;
+    });
+
+    total.textContent = suma;
+    contador.textContent = carrito.length;
+}
+
+function vaciarCarrito() {
+    carrito = [];
+    actualizarCarrito();
+}
+
+function cerrarCarrito() {
+    carrito = [];
+    actualizarCarrito();
+
+}
+
+document.getElementById("carrito-btn").addEventListener("click", () => {
+    document.getElementById("carrito").classList.toggle("oculto");
+});
+
+
+function realizarPago() {
+    if (carrito.length === 0) {
+        alert("El carrito está vacío ❌");
+        return;
+    }
+
+    pedidos.push([...carrito]);
+    mostrarPedidos();
+
+    alert("Pago realizado con éxito 💳");
+
+    carrito = [];
+    actualizarCarrito();
+}
+
+
+function mostrarPedidos() {
+    const lista = document.getElementById("lista-pedidos");
+    lista.innerHTML = "";
+
+    pedidos.forEach((pedido, i) => {
+        lista.innerHTML += `<li>Pedido ${i + 1} - ${pedido.length} productos</li>`;
+    });
+}
+function mostrarProductos(lista) {
+    contenedor.innerHTML = "";
+
+   mostrarProductos(productos);
+}
+function mostrarProductos(lista) {
+    contenedor.innerHTML = "";
+
+    lista.forEach(p => {
+        contenedor.innerHTML += `
+            <div class="producto">
+                <img src="${p.img}">
+                <h3>${p.nombre}</h3>
+                <p>$${p.precio}</p>
+                <button onclick="agregarCarrito(${p.id})">Comprar</button>
+            </div>
+        `;
+    });
+}
+if (inputBusqueda) {
+    inputBusqueda.addEventListener("input", () => {
+        const texto = inputBusqueda.value.toLowerCase();
+
+        const filtrados = productos.filter(p =>
+            p.nombre.toLowerCase().includes(texto)
+        );
+
+        mostrarProductos(filtrados);
+    });
+}
+mostrarProductos(productos);
